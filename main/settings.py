93023ad257e5baa4a42f0486e2ba052b2b6bd2a5
dhanus <dhanus@mit.edu>
2019-02-13 13:10:55 -0400

    add staticfiles_dir with updated path in settings.py

--- main/settings.py
+++ main/settings.py
@@ -124,9 +124,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATIC_URL = '/static/'
+]
 
 STATICFILES_DIRS = [
-    "home/static/home/",
+   os.path.join(BASE_DIR,'static') + '/home'
 ]
 
 
