594a41ec6a3c64a1e3e89ed2129245a5a12e8b99
dhanus <dhanus@mit.edu>
2019-02-14 18:32:18 -0400

    add jinja2

--- main/settings.py
+++ main/settings.py
@@ -56,6 +56,14 @@ ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
     {
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+         'DIRS': [],
+         'APP_DIRS': True,
+         'OPTIONS': {
+            'environment': 'main.jinja2.environment'
+             },
+         },
+    {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
