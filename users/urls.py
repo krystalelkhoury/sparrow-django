80b742888a4ad9c3c39b6f8562ecddaa59dfaa03
dhanus <dhanus@mit.edu>
2019-02-15 14:20:46 -0400

    update URL patterns with static stuff

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('start/', views.Start.as_view(), name='start'),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('login/', views.Login.as_view(), name='login'),
+    path('passwordreset/', views.PasswordReset.as_view(), name='passwordreset'),
     path('a/dashboard/', views.AdminDashboard.as_view(), name='adashboard'),
     path('e/dashboard/', views.EmployeeDashboard.as_view(), name='edashboard'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
